f68efe026cf609440a55ad7a1c9c951421e82381
Zeelpatel-dev <140406658+Zeelpatel-dev@users.noreply.github.com>
2023-07-31 18:37:40 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
+from sketchpy import canvas
 import turtle
-from sketchpy import library as lib
 
-screen = turtle.Screen()
-screen.bgcolor("blue")
 
-web = turtle.Turtle()
-web.speed(0)
-web.penup()
-web.goto(-200, 150)
-web.pendown()
+t = turtle.Turtle()
+t.speed(0)
+t.hideturtle()
 
-for i in range(72):
-    web.goto(-200, 150)
-    web.right(5)
-    web.forward(150)
 
-web.penup()
-web.goto(-275, 125)
-web.pendown()
-for i in range(180):
-    web.right(5)
-    web.forward(2.5)
+t.penup()
+t.goto(-300, 300)
+t.pendown()
+t.fillcolor("blue")
+t.begin_fill()
+for _ in range(4):
+    t.forward(600)
+    t.right(90)
+t.end_fill()
 
-obj = lib.tom_holland()
+
+t.penup()
+t.goto(0, 100)
+t.pendown()
+t.pensize(5)
+t.color("navy")
+for _ in range(24):
+    t.forward(75)
+    t.backward(75)
+    t.left(15)
+
+
+obj = canvas.sketch_from_svg(r"C:\Users\Lenovo\Downloads\2023_07_30.svg")
 obj.draw()
-turtle.exitonclick()
+
+turtle.done()
