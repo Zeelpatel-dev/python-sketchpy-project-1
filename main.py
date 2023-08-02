ba9bb4af626c293488a921224cda9f031099bf98
Zeelpatel-dev <140406658+Zeelpatel-dev@users.noreply.github.com>
2023-08-02 16:31:32 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,35 +1,13 @@
+from sketchpy import library as lib
+
 from sketchpy import canvas
-import turtle
+#obj = canvas.sketch_from_image(r"C:\Users\Lenovo\Downloads\download.jpg")
+#obj.draw(threshold = 127)
 
 
-t = turtle.Turtle()
-t.speed(0)
-t.hideturtle()
+#obj = lib.rdj()
+#obj.draw()
 
-
-t.penup()
-t.goto(-300, 300)
-t.pendown()
-t.fillcolor("blue")
-t.begin_fill()
-for _ in range(4):
-    t.forward(600)
-    t.right(90)
-t.end_fill()
-
-
-t.penup()
-t.goto(0, 100)
-t.pendown()
-t.pensize(5)
-t.color("navy")
-for _ in range(24):
-    t.forward(75)
-    t.backward(75)
-    t.left(15)
-
-
-obj = canvas.sketch_from_svg(r"C:\Users\Lenovo\Downloads\2023_07_30.svg")
-obj.draw()
-
-turtle.done()
+from sketchpy import library
+myObject = library.ironman_ascii()
+myObject.draw()
