820f5adac13a4fec3dfa2eb961a868a4019f3801
Zeelpatel-dev <140406658+Zeelpatel-dev@users.noreply.github.com>
2023-08-03 16:23:48 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
 from sketchpy import library as lib
-
+import turtle
 from sketchpy import canvas
-#obj = canvas.sketch_from_image(r"C:\Users\Lenovo\Downloads\download.jpg")
-#obj.draw(threshold = 127)
+
+
+screen = turtle.Screen()
+screen.bgcolor("blue")
+
+
+chakra = turtle.Turtle()
+chakra.speed(0)
+chakra.penup()
+chakra.setpos(-50, 100)
+chakra.pendown()
+
+
+chakra.pensize(2)
+chakra.pencolor("yellow")
+for i in range(24):
+    chakra.forward(60)
+    chakra.backward(60)
+    chakra.left(15)
+
+
+chakra.hideturtle()
+
+
+obj = canvas.sketch_from_image(r"C:\Users\Lenovo\Downloads\24052021104050zzq0.jpg")
+obj.draw(threshold=127)
+
+
+turtle.mainloop()
 
 
 #obj = lib.rdj()
 #obj.draw()
 
-from sketchpy import library
-myObject = library.ironman_ascii()
-myObject.draw()
+#from sketchpy import library
+#myObject = library.ironman_ascii()
+#myObject.draw()
